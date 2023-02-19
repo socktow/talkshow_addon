1f18b58104854be6446bbcec39bee6778b25ed9b
MsZihu <mjssdn95@gmail.com>
2023-02-20 03:39:35 +0700

    Update joinguild-demo.js

--- taixiu.js
+++ taixiu.js
@@ -1,65 +1,62 @@
-const slotItems = [
-  "<:1:879665246556545086>", 
-  "<:2:879665246808191016>", 
-  "<:3:879665246506205186>", 
-  "<:4:879665246569115649>", 
-  "<:5:879665246686568469>",
-  "<:6:879665246376185887>"
-];
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const guildId = '1'; // ID của Guild A
+const roleId = '1995'; // ID của role trong Guild A
+const ownerId = '1998'; // ID của chủ sở hữu guild
+const joinCommand = '!joinguild'; // Lệnh để tham gia vào Guild A
 
-  module.exports = {
-    
-          name:"taixiu",
-          aliases: ["taixiu","tx"],
-          category: "economy",
-          description: "taixiu",
-          usage: "taixiu",
-          accessableby: ""
-      ,
+client.on('message', async message => {
+  if (message.content.toLowerCase() === joinCommand.toLowerCase()) {
+    // Nếu member nhập lệnh "!joinguild"
+    const filter = m => m.author.id === message.author.id;
+    const options = { max: 1, time: 60000, errors: ['time'] };
+    // Tạo form để nhập thông tin
+    message.channel.send('Vui lòng nhập tên của bạn: ');
+    const name = (await message.channel.awaitMessages(filter, options)).first().content;
+    message.channel.send('Vui lòng nhập tuổi của bạn: ');
+    const age = (await message.channel.awaitMessages(filter, options)).first().content;
 
-      run: async (bot, message, args) => {
-  
-      if(!message.member.hasPermission("MANAGE_MESSAGES")) 
-      return message.reply("Bạn không phải host sòng.").then(message=>message.delete({timeout:"5000"}));
-
-      if(!message.guild.me.hasPermission("MANAGE_MESSAGES")) 
-      return message.reply("Tui không có quyền chỉnh sửa tin nhắn.").then(message=>message.delete({timeout:"5000"}));
-
-      let user = message.author;
-
-      let number = []
-      for (let i = 0; i < 3; i++) { number[i] = Math.floor(Math.random() * slotItems.length); }
-
-      message.channel.send(` các con nghiện ! Sẵn sàng chưa nào`)
-        
-          //Bắt đầu roll
-          .then((msg) => {
-            setTimeout(function() {
-                msg.edit(`<a:z1:879671558749167626> <a:z1:879671558749167626> <a:z1:879671558749167626>`);
-              }, 3000);
-           // roll 1
-            setTimeout(function() {
-            msg.edit(`${slotItems[number[0]]} <a:z1:879671558749167626> <a:z1:879671558749167626>`);
-          }, 6000);
-          // roll ra 2
-          setTimeout(function() {
-            msg.edit(`${slotItems[number[0]]} ${slotItems[number[1]]} <a:z1:879671558749167626>`);
-          }, 9000);
-          // roll ra 3
-          setTimeout(function() {
-            msg.edit(`${slotItems[number[0]]} ${slotItems[number[1]]} ${slotItems[number[2]]}`);
-          }, 12000);
-        })
-        
-        // MESSAGE BẮT ĐẦU - KẾT THÚC
-        message.channel.send(`**Bỏ cái tay ra nghennnnnn** `)
-            .then((msg) => {
-            setTimeout(function() {
-                msg.edit(`**Lắc Nè Quý Zị**`);
-              }, 3000);
-            setTimeout(function() {
-                msg.edit(`**Tổng** : `);
-              }, 12000)})
-  
+    // Tạo Embed để gửi tin nhắn tới chủ sở hữu guild
+    const embed = new Discord.MessageEmbed()
+      .setTitle('Thông tin đăng ký')
+      .addField('Tên', name)
+      .addField('Tuổi', age)
+      .setTimestamp();
+    const owner = message.guild.members.cache.get(ownerId);
+    owner.send(embed)
+      .then(msg => {
+        msg.react('✅'); // Thêm emoji cho tin nhắn
+        msg.react('❌');
+        const filter = (reaction, user) => {
+          return ['✅', '❌'].includes(reaction.emoji.name) && user.id === ownerId;
+        };
+        const collector = msg.createReactionCollector(filter, { max: 1, time: 60000 });
+        collector.on('collect', (reaction, user) => {
+          if (reaction.emoji.name === '✅') {
+            // Nếu chủ guild chọn ✅ thì member sẽ được thêm vào guild và được thêm role
+            const member = message.member;
+            const guild = client.guilds.cache.get(guildId);
+            guild.members.add(member)
+              .then(() => {
+                const role = guild.roles.cache.get(roleId);
+                member.roles.add(role);
+                message.channel.send(`Chúc mừng, bạn đã được thêm vào guild ${guild.name}!`);
+              })
+              .catch(error => {
+                console.error(error);
+                message.channel.send('Có lỗi xảy ra khi thêm bạn vào guild. Vui lòng thử lại sau.');
+              });
+          } else {
+            // Nếu chủ guild chọn ❌ thì không thêm member vào guild
+            message.channel.send('Đăng ký của bạn đã bị từ chối.');
+          }
+        });
+      })
+      .catch(error => {
+        console.error(error);
+        message.channel.send('Có lỗi xảy ra khi gửi tin nhắn tới chủ guild. Vui lòng thử lại sau.');
+      });
   }
-  }
+});
+
+client.login('YOUR_DISCORD_BOT_TOKEN');
